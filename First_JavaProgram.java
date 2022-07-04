951dc66f0553720e0fb05e79bb47201f6d6aaa40
dhjCoding <92060457+dhjCoding@users.noreply.github.com>
2022-07-04 21:42:00 +0200

    Create First_JavaProgram.java

---
+++ First_JavaProgram.java
@@ -0,0 +1,13 @@
+
+package first_javaprogram;
+
+
+public class First_JavaProgram {
+
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("This is my first java program");
+    }
+    
+}
